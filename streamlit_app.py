939f98eaf868477341ba1a1df7f4e0b73ad83b02
Demo12-droid <ibi632024@gmail.com>
2024-07-23 15:48:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     # Display the uploaded image
-    st.image(uploaded_file, caption='Uploaded Image', use_column_width=False)
+    st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
 
     # Placeholder for the second image (you can replace this with your own image path)
     second_image = uploaded_file
